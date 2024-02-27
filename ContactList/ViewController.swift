d0a7113fd202c9e17a2447adcbbd291528ad24d6
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-02-27 15:45:38 +0300

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Tatiana Lazarenko on 2/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
